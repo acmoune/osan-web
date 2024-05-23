578c2523e6e01fa3ac6e1495b0fc831f7158ce94
Alain Moune <ac.moune@gmail.com>
2024-05-23 15:52:53 +0100

    Added Delivery options

---
+++ src/OsanWebsite.Web/Pages/CardsPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OsanWebsite.Web.Pages
+{
+    public class CardsPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ src/OsanWebsite.Web/Pages/DeliveryPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OsanWebsite.Web.Pages
+{
+    public class DeliveryPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
