3189465742717494cb148e369793caa2b8ca7e7c
Alain Moune <ac.moune@gmail.com>
2024-06-05 15:37:50 +0100

    fixed next event

--- src/OsanWebsite.Web/Pages/BookingPage.cshtml.cs
+++ src/OsanWebsite.Web/Pages/BookingPage.cshtml.cs
@@ -6,10 +6,12 @@ namespace OsanWebsite.Web.Pages
     public class BookingPageModel : PageModel
     {
         public string? CampaignId { get; set; }
+        public string? EventSlug { get; set; }
 
-        public void OnGet(string? campaign)
+        public void OnGet(string? campaign, string? slug)
         {
             CampaignId = campaign;
+            EventSlug = slug;
         }
     }
 }
--- src/OsanWebsite.Web/Pages/Index.cshtml.cs
+++ src/OsanWebsite.Web/Pages/Index.cshtml.cs
@@ -7,19 +7,24 @@ namespace OsanWebsite.Web.Pages;
 public class IndexModel : PageModel
 {
     public string Feed { get; set; } = default!;
+    public Event? NextEvent { get; set; }
+
     private IPicturesRepo _picturesRepo;
+    private IEventsRepository _eventsRepository;
 
     public PictureSet SlideShow { get; set; } = default!;
     public PictureSet Galery { get; set; } = default!;
 
-    public IndexModel(IPicturesRepo picturesRepo)
+    public IndexModel(IPicturesRepo picturesRepo, IEventsRepository eventsRepository)
     {
         _picturesRepo = picturesRepo;
+        _eventsRepository = eventsRepository;
     }
 
     public async Task OnGetAsync(string? feed)
     {
         Feed = feed ?? "spotlights";
         SlideShow = await _picturesRepo.GetSlides();
+        NextEvent = await _eventsRepository.GetNextEventAsync();
     }
 }
